2f431989394d6067865e98a4cd0909735749e0b5
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-06-27 08:09:09 -0400

    light dark theme cont

--- react-admin/src/theme.js
+++ react-admin/src/theme.js
@@ -75,7 +75,7 @@ blueAccent: {
         100: "#d0d1d5",
         200: "#a1a4ab",
         300: "#727681",
-        400: "#434957",
+        400: "#f2f0f0",
         500: "#141b2d",
         600: "#101624",
         700: "#0c101b",
@@ -117,3 +117,77 @@ blueAccent: {
     },
 }),
 });
+
+//mui theme settings
+
+export const themeSetttings = (mode) => {
+    const colors = tokens(mode);
+
+    return {
+        palette: {
+            mode: mode,
+            ...(mode = 'dark'
+            ? {
+                primary: {
+                    main: colors.primary[500],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: colors.primary[500],
+                }
+            } : {
+                primary: {
+                    main: colors.primary[100],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: "#fcfcfc",
+                } 
+            }
+            ),
+        },
+        typography: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+
+        }
+    }
+}
