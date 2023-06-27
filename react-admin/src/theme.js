502b091f81dd60dbc7bd74f79d0d1c084a83308a
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-06-27 08:23:18 -0400

    fix comma

--- react-admin/src/theme.js
+++ react-admin/src/theme.js
@@ -206,7 +206,7 @@ export const useMode = () => {
     const colorMode = useMemo(
         () => ({
             toggleColorMode: () => 
-                setMode((prev) => (prev === "light" ? "dark" : "light"));
+                setMode((prev) => (prev === "light" ? "dark" : "light")),
             
         }),
         []
