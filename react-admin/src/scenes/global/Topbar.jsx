c2ada209bbd1ab1decf151c998cd37aac78b786a
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-06-28 06:38:45 -0400

    top bar creation

--- react-admin/src/scenes/global/Topbar.jsx
+++ react-admin/src/scenes/global/Topbar.jsx
@@ -1,7 +1,33 @@
-
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext,tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationOutlinedIcon from "@mui/icons-material/NotificationOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Topbar = () => {
-    return <div>Topbar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+    return <Box display="flex" justifyContent="space-between" p={2}>
+        {/* Search Bar */}
+        <Box display="flex" 
+        backgroundColor={colors.primary[400]} 
+        borderRadius= "3px"
+        >
+            <InputBase sx={{ml: 2, flex: 1}} placeholder="Search"/>
+            <IconButton type="button" sx={{p: 1}}>
+                <SeachIcon/>
+            </IconButton>
+        </Box>
+
+        <IconButton ></IconButton>
+    </Box>
 }
 
 export default Topbar;
